b206e99b48cc86c5847a5112cf9441e762e9e58d
Sajal <sirohisajal@gmail.com>
2020-09-24 23:19:40 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 from pinpoint_campaign_builder import PinpointCampaignBuilder
 
 pp = PinpointCampaignBuilder(channel_type=["EMAIL", "SMS"],
-                            pinpoint_access_role_arn='arn:aws:iam::392658218916:role/s3-pinpoint-acess',
-                            ses_identity_arn='arn:aws:ses:ap-south-1:392658218916:identity/reachus@konfhub.com')
+                            pinpoint_access_role_arn='arn:aws:iam::{some_id}:role/s3-pinpoint-acess',
+                            ses_identity_arn='arn:aws:ses:{your-region}:{some_id}:identity/{email_id}')
 
 pp.delete_all_apps()
